ff96f1c2781d377ca93b871f8c9cc08de8795e80
Bennett DenBleyker <b.denbleyker@comcast.net>
2024-08-26 21:31:09 -0600

    Unwrap try-except blocks
    Removes implementation details (*how* exactly the bit is being resolved) from the exception traceback.
     - Increases readability
     - Removes 21 lines of useless traceback (~80%)

--- django/template/base.py
+++ django/template/base.py
@@ -867,6 +867,60 @@ class Variable:
     def __str__(self):
         return self.var
 
+    RESOLVE_FAILED = object()
+    
+    def _resolve_callable(self, current):
+        if getattr(current, "do_not_call_in_templates", False):
+            return current
+        elif getattr(current, "alters_data", False):
+            return context.template.engine.string_if_invalid
+        else:
+            try:  # method call (assuming no args required)
+                current = current()
+            except TypeError:
+                try:
+                    signature = inspect.signature(current)
+                except ValueError:  # No signature found.
+                    return context.template.engine.string_if_invalid
+                else:
+                    try:
+                        signature.bind()
+                    except TypeError:  # Arguments *were* required.
+                        # Invalid method call.
+                        return context.template.engine.string_if_invalid
+                    else:
+                        raise
+        return current
+        
+    def _resolve_next(self, current, bit):
+        try:  # dictionary lookup
+            return current[bit]
+            # ValueError/IndexError are for numpy.array lookup on
+            # numpy < 1.9 and 1.9+ respectively
+        except (TypeError, AttributeError, KeyError, ValueError, IndexError):
+            pass
+        try:  # attribute lookup
+            # Don't return class attributes if the class is the context:
+            if isinstance(current, BaseContext) and getattr(
+                type(current), bit
+            ):
+                raise AttributeError
+            return getattr(current, bit)
+        except (TypeError, AttributeError):
+            # Reraise if the exception was raised by a @property
+            if not isinstance(current, BaseContext) and bit in dir(current):
+                raise
+        try:  # list-index lookup
+            return current[int(bit)]
+        except (
+            IndexError,  # list index out of range
+            ValueError,  # invalid literal for int()
+            KeyError,  # current is a dict without `int(bit)` key
+            TypeError,
+        ):  # unsubscriptable object
+            pass
+        return self.RESOLVE_FAILED
+        
     def _resolve_lookup(self, context):
         """
         Perform resolution of a real variable (i.e. not a literal) against the
@@ -879,55 +933,14 @@ class Variable:
         current = context
         try:  # catch-all for silent variable failures
             for bit in self.lookups:
-                try:  # dictionary lookup
-                    current = current[bit]
-                    # ValueError/IndexError are for numpy.array lookup on
-                    # numpy < 1.9 and 1.9+ respectively
-                except (TypeError, AttributeError, KeyError, ValueError, IndexError):
-                    try:  # attribute lookup
-                        # Don't return class attributes if the class is the context:
-                        if isinstance(current, BaseContext) and getattr(
-                            type(current), bit
-                        ):
-                            raise AttributeError
-                        current = getattr(current, bit)
-                    except (TypeError, AttributeError):
-                        # Reraise if the exception was raised by a @property
-                        if not isinstance(current, BaseContext) and bit in dir(current):
-                            raise
-                        try:  # list-index lookup
-                            current = current[int(bit)]
-                        except (
-                            IndexError,  # list index out of range
-                            ValueError,  # invalid literal for int()
-                            KeyError,  # current is a dict without `int(bit)` key
-                            TypeError,
-                        ):  # unsubscriptable object
-                            raise VariableDoesNotExist(
-                                "Failed lookup for key [%s] in %r",
-                                (bit, current),
-                            )  # missing attribute
-                if callable(current):
-                    if getattr(current, "do_not_call_in_templates", False):
-                        pass
-                    elif getattr(current, "alters_data", False):
-                        current = context.template.engine.string_if_invalid
-                    else:
-                        try:  # method call (assuming no args required)
-                            current = current()
-                        except TypeError:
-                            try:
-                                signature = inspect.signature(current)
-                            except ValueError:  # No signature found.
-                                current = context.template.engine.string_if_invalid
-                            else:
-                                try:
-                                    signature.bind()
-                                except TypeError:  # Arguments *were* required.
-                                    # Invalid method call.
-                                    current = context.template.engine.string_if_invalid
-                                else:
-                                    raise
+                current = self._resolve_next(current, bit)
+                if current is self.RESOLVE_FAILED:
+                    raise VariableDoesNotExist(
+                        "Failed lookup for key [%s] in %r",
+                        (bit, current),
+                    )  # missing attribute
+                elif callable(current):
+                    current = self._resolve_callable(current)
         except Exception as e:
             template_name = getattr(context, "template_name", None) or "unknown"
             logger.debug(
